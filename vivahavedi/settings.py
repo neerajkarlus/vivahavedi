023f3e8fb7370391af419b3ca016042cd468df2c
vivahavedi <neerajkarlus1997@gmail.com>
2021-07-01 20:56:07 +0530

    added environ

--- vivahavedi/settings.py
+++ vivahavedi/settings.py
@@ -136,6 +136,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
