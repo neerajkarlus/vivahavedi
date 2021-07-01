1555f3bcb9e5ff8a03fd834db91c483e4843d8cb
vivahavedi <neerajkarlus1997@gmail.com>
2021-07-01 17:24:59 +0530

    added database con

--- vivahavedi/settings.py
+++ vivahavedi/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default='postgres://...'}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
