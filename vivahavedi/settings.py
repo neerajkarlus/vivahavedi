64bd1ca86864bbe6768553e13a43cb90120e87fe
vivahavedi <neerajkarlus1997@gmail.com>
2021-07-03 13:26:05 +0530

    added environ latest

--- vivahavedi/settings.py
+++ vivahavedi/settings.py
@@ -19,10 +19,8 @@ env = environ.Env(
      DEBUG=(bool, False)
 )
 
-environ.Env.read_env()
 #environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG=env('DEBUG')
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
